9bda59b13632a9e0bea78f9d3297ed7788aef71b
huniversal <hunivers0523@naver.com>
2026-05-10 16:40:04 +0900

    init: eslint 세팅

---
+++ eslint/react.js
@@ -0,0 +1,62 @@
+import globals from 'globals'
+import pluginReact from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+
+export const config = [
+  pluginReact.configs.flat.recommended,
+  {
+    settings: { react: { version: 'detect' } },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ...pluginReact.configs.flat.recommended.languageOptions,
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^react$', '^@?\\w'],
+            ['^@kareer/'],
+            [
+              '^@app/',
+              '^@pages/',
+              '^@widgets/',
+              '^@features/',
+              '^@entities/',
+              '^@shared/',
+            ],
+            ['^\\u0000'],
+            ['^\\.'],
+            ['^.+\\.css$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+      'no-console': 'warn',
+      curly: ['error', 'all'],
+    },
+  },
+]
