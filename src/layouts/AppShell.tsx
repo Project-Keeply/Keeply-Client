e18b347077ac9511914968ca77851b33aaf3ac2e
huniversal <hunivers0523@naver.com>
2026-05-12 01:03:48 +0900

    fix: 레이아웃 잘림 변경

--- src/layouts/AppShell.tsx
+++ src/layouts/AppShell.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router'
 
 const AppShell = () => {
   return (
-    <div className="mx-auto flex h-full max-w-[430px] flex-col bg-white pb-[env(safe-area-inset-bottom)] pt-[env(safe-area-inset-top)]">
+    <div className="mx-auto flex min-h-screen min-h-[100dvh] max-w-[430px] flex-col bg-white pb-[env(safe-area-inset-bottom)] pl-[env(safe-area-inset-left)] pr-[env(safe-area-inset-right)] pt-[env(safe-area-inset-top)]">
       <header className="shrink-0">Header</header>
       <main className="flex-1 overflow-y-auto">
         <Outlet />
