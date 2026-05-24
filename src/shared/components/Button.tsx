a8806d8b2f1ddc6491aac3092c6e924594140f7d
huniversal <hunivers0523@naver.com>
2026-05-25 01:14:48 +0900

    feat: disabled 분리

--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -1,21 +1,22 @@
 import type { ButtonHTMLAttributes, ReactNode } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant: 'primary' | 'gray' | 'red';
+  variant: 'primary' | 'red';
   children: ReactNode;
 }
 
 const variantStyles = {
-  primary: 'bg-primary-500 text-white hover:bg-primary-600',
-  gray: 'bg-gray-200 text-white hover:bg-gray-300',
-  red: 'bg-red-500 text-white hover:bg-red-600',
+  primary: 'bg-primary-500 text-white hover:bg-primary-600 cursor-pointer ',
+  disabled: 'bg-gray-200 text-white',
+  red: 'bg-red-500 text-white hover:bg-red-600 cursor-pointer ',
 };
 
 const Button = ({ variant, children, className, ...rest }: ButtonProps) => {
   return (
     <button
       type="button"
-      className={`w-full py-4.5 rounded-[10px] text-button1 transition-colors cursor-pointer focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-500 ${variantStyles[variant]} ${className ?? ''}`}
+      disabled={rest.disabled}
+      className={`w-full py-4.5 rounded-[10px] text-button1 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-500 ${rest.disabled ? variantStyles.disabled: variantStyles[variant]} ${className ?? ''}`}
       {...rest}
     >
       {children}
