d3008238093301f52f042b5d019d4153b20d59c3
huniversal <hunivers0523@naver.com>
2026-05-25 00:57:28 +0900

    feat: Button 공통 컴포넌트 구현

---
+++ src/shared/components/Button.tsx
@@ -0,0 +1,26 @@
+import type{ ReactNode } from 'react';
+
+interface ButtonProps {
+  variant: 'primary' | 'gray' | 'red'
+  children: ReactNode;
+  onClick?: () => void;
+}
+
+const variantStyles = {
+  primary: 'bg-primary-500 text-white hover:bg-primary-600',
+  gray: 'bg-gray-200 text-white hover:bg-gray-300',
+  red: 'bg-red-500 text-white hover:bg-red-600',
+};
+
+const Button = ({ variant, children, onClick }: ButtonProps) => {
+  return (
+    <button
+      className={`w-full py-4.5 rounded-[10px] text-button1 transition-colors cursor-pointer ${variantStyles[variant]}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button
