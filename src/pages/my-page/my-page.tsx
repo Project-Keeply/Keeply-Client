44b82852fa1dd28dd185c4ab918a4c943297b9b4
huniversal <hunivers0523@naver.com>
2026-05-25 18:19:44 +0900

    feat: api 확장 및 폴백 로직 처리

--- src/pages/my-page/my-page.tsx
+++ src/pages/my-page/my-page.tsx
@@ -1,5 +1,11 @@
-const MyPage = () => {
-  return <></>
-}
+import { CommonHeader } from '@shared/components';
 
-export default MyPage
+const MyPage = () => {
+  return (
+    <>
+      <CommonHeader title="마이페이지" showBack={true} />
+    </>
+  );
+};
+
+export default MyPage;
