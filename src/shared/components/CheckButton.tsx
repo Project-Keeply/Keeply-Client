34ead869ea7bc78a2ce74bc482d46583537dd0ef
huniversal <hunivers0523@naver.com>
2026-05-25 17:32:12 +0900

    feat: 체크 버튼 컴폰너트 구현

--- src/shared/components/CheckButton.tsx
+++ src/shared/components/CheckButton.tsx
@@ -1,9 +1,33 @@
-const CheckButton = () => {
+import type { ReactNode } from 'react';
+
+import { IcCheck, IcFillCheck } from '../icons/svgs';
+
+interface CheckButtonProps {
+  children: ReactNode;
+  isChecked?: boolean;
+  size?: 'sm' | 'lg';
+  onClick: () => void;
+  hasBackground?: boolean;
+}
+
+const CheckButton = ({ children, isChecked = false, size = 'lg', onClick, hasBackground = false }: CheckButtonProps) => {
   return (
-    <div>
-      
-    </div>
-  )
+    <button
+      type="button"
+      onClick={onClick}
+      className={`w-full px-4.5 flex items-center justify-between rounded-[10px] transition-colors 
+        ${!hasBackground ? 'bg-white' : isChecked ? 'bg-primary-200' : 'bg-gray-100'} ${size === 'sm' ? 'py-5' : 'py-8'}`}
+    >
+      <span
+        className={
+          size === 'sm' ? 'text-body4 font-semibold' : 'text-title1 font-semibold'
+        }
+      >
+        {children}
+      </span>
+      {isChecked ? <IcFillCheck /> : <IcCheck />}
+    </button>
+  );
 }
 
 export default CheckButton
