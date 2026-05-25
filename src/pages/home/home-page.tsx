fe8ec11c007147e9b6a3ef8d93c5c4ef51a1b208
huniversal <hunivers0523@naver.com>
2026-05-25 16:48:17 +0900

    fix: 테스트 코드 제거

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,13 +1,7 @@
-import { BirdIcon } from '@shared/icons';
-
 const HomePage = () => {
   return (
     <div className="flex flex-col gap-2 p-4">
       <h1 className="text-2xl font-bold">홈 페이지</h1>
-      <div className="flex items-center gap-2 text-gray-700">
-        <BirdIcon width={20} height={20} />
-        <span>SVGR 아이콘 적용 완료</span>
-      </div>
     </div>
   );
 }
