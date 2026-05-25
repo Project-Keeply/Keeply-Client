40478f0e9ccbe78320ee78713bd4467e6bcf2268
huniversal <hunivers0523@naver.com>
2026-05-25 18:17:22 +0900

    feat: 공통 헤더 컴포넌트 구현

--- src/shared/components/CommonHeader.tsx
+++ src/shared/components/CommonHeader.tsx
@@ -1,9 +1,27 @@
-const CommonHeader = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import { IcLeftArrow } from '../icons/svgs';
+
+interface CommonHeaderProps {
+  title: string;
+  showBack?: boolean;
 }
+
+const navigateBack = () => {
+  window.history.back();
+}
+
+const CommonHeader = ({ title, showBack }: CommonHeaderProps) => {
+  return (
+    <header className="relative flex h-[56px] items-center justify-center">
+      {showBack && (
+        <button 
+          type="button"
+          className="left-4.75 absolute" onClick={navigateBack}>
+          <IcLeftArrow />
+        </button>
+      )}
+      <h1 className="text-title2 font-semibold">{title}</h1>
+    </header>
+  );
+};
 
 export default CommonHeader
