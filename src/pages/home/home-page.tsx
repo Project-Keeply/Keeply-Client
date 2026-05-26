e36700b7c00acd7936da20fba28128423fc41e9f
huniversal <hunivers0523@naver.com>
2026-05-26 17:59:06 +0900

    feat: 홈페이지 테스트 코드 제거

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,25 +1,7 @@
-import { useState } from 'react';
-import { Input, TextArea } from '@shared/components';
-
 const HomePage = () => {
-  const [name, setName] = useState('');
-  const [content, setContent] = useState('');
-
   return (
     <div className="flex flex-col gap-8 p-10">
-      <section className="flex flex-col gap-2">
-        <Input value={name} placeholder="이름" onChange={setName} />
-      </section>
-
-      <section className="flex flex-col gap-2">
-        <TextArea
-          title="내용"
-          explanation="(상세 내용은 필수 사항이 아닙니다.)"
-          value={content}
-          placeholder="공지사항 내용을 남겨주세요"
-          onChange={setContent}
-        />
-      </section>
+      홈페이지
     </div>
   );
 };
