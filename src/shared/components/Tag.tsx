630989ea342ed830c9e349b1fda069727253e265
huniversal <hunivers0523@naver.com>
2026-05-26 14:13:08 +0900

    feat: Tag 컴포넌트 구현

---
+++ src/shared/components/Tag.tsx
@@ -0,0 +1,19 @@
+interface TagProps {
+  variant: 'primary' | 'secondary';
+  label : string;
+}
+
+const variantStyles = {
+  primary: 'bg-primary-100 text-primary-500',
+  secondary: 'bg-orange-100 text-orange-500',
+}
+
+const Tag = ({ variant, label }: TagProps) => {
+  return (
+    <span className={`inline-flex items-center rounded-[5px] px-2.25 py-1.5 ${variantStyles[variant]} text-label2 font-semibold`}>
+      {label}
+    </span>
+  )
+};
+
+export default Tag
--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button';
 export { default as CheckButton } from './CheckButton';
 export { default as CommonHeader } from './CommonHeader';
+export { default as Tag } from './Tag';
