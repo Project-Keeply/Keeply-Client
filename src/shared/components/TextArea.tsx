3a266d53db60cf722068309e8df269c6e0f7c183
huniversal <hunivers0523@naver.com>
2026-05-26 17:29:25 +0900

    feat: textarea 컴포넌트 구현

--- src/shared/components/TextArea.tsx
+++ src/shared/components/TextArea.tsx
@@ -1,9 +1,37 @@
-const TextArea = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+interface TextAreaProps {
+  title: string;
+  explanation?: string;
+  content: string;
+  placeholder?: string;
+  onChange: (value: string) => void;
 }
+
+const TextArea = ({
+  title,
+  explanation,
+  content,
+  placeholder,
+  onChange,
+}: TextAreaProps) => {
+  return (
+    <>
+      <div>
+        <div className="flex items-baseline gap-1.5 pb-5">
+          <h2 className="text-title3 text-gray-800 font-semibold">{title}</h2>
+          <p className="relative text-body1 font-light text-gray-200">
+            {explanation}
+            <span className="absolute -top-0.5 -right-2 w-1.5 h-1.5 rounded-full bg-orange-500" />
+          </p>
+        </div>
+        <textarea
+          value={content}
+          placeholder={placeholder}
+          onChange={(e) => onChange(e.target.value)}
+          className="w-full h-40 bg-transparent outline-none placeholder:text-gray-200 text-title3 font-normal pb-2 border-b border-gray-100"
+        />
+      </div>
+    </>
+  );
+};
 
 export default TextArea
