738de14096b67270a3778d2af8e6ed5010a8c776
huniversal <hunivers0523@naver.com>
2026-06-19 20:30:20 +0900

    fix: variant props 추가

--- src/entities/working-space/components/WorkingLog.tsx
+++ src/entities/working-space/components/WorkingLog.tsx
@@ -3,14 +3,15 @@ import Tag from '@shared/components/Tag';
 interface WorkingLogProps {
   tag: string;
   content: string;
+  variant: 'primary' | 'secondary';
 }
 
 
-const WorkingLog = ({ tag, content }: WorkingLogProps) => {
+const WorkingLog = ({ tag, content, variant }: WorkingLogProps) => {
   return (
     <div className='flex items-start gap-5 px-5 py-5 bg-white'>
       <span className="shrink-0">
-        <Tag variant="primary" label={tag} />
+        <Tag variant={variant} label={tag} />
       </span>
       <p className="text-body2 text-gray-800">{content}</p>
     </div>
