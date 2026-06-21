9632324b02c04a796a740168d33d7134cccd849c
huniversal <hunivers0523@naver.com>
2026-06-21 22:17:24 +0900

    fix: explanation 없을 때 주황 점 조건부 렌더링 처리

--- src/shared/components/WritePageTitle.tsx
+++ src/shared/components/WritePageTitle.tsx
@@ -8,10 +8,12 @@ const WritePageTitle = ({ title, explanation }: WritePageTitleProps) => {
     <div>
       <label className="flex items-baseline gap-1.5 pb-5">
         <span className="text-title3 text-gray-800 font-semibold">{title}</span>
-        <span className="relative text-body1 font-light text-gray-200">
-          {explanation}
-          <span className="absolute -top-0.5 -right-2 w-1.5 h-1.5 rounded-full bg-orange-500" />
-        </span>
+        {explanation && (
+          <span className="relative text-body1 font-light text-gray-200">
+            {explanation}
+            <span className="absolute -top-0.5 -right-2 w-1.5 h-1.5 rounded-full bg-orange-500" />
+          </span>
+        )}
       </label>
     </div>
   )
