353fc376fc81d1a2affbdb271ce68ef33629bd40
huniversal <hunivers0523@naver.com>
2026-06-21 23:22:25 +0900

    fix: 불필요 div 제거

--- src/shared/components/TagButton.tsx
+++ src/shared/components/TagButton.tsx
@@ -6,12 +6,14 @@ interface TagButtonProps {
 
 const TagButton = ({ label, isSelected, onClick }: TagButtonProps) => {
   return (
-    <div className="shrink-0">
-      <button className={`px-8 py-4 rounded-[5px] text-body1 font-medium ${isSelected ? 'bg-primary-500 text-white' : 'bg-gray-100 text-gray-300'}`} onClick={onClick}>
-        {label}
-      </button>
-    </div>
-  )
-}
+    <button
+      type="button"
+      className={`shrink-0 px-8 py-4 rounded-[5px] text-body1 font-medium ${isSelected ? 'bg-primary-500 text-white' : 'bg-gray-100 text-gray-300'}`}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
 
-export default TagButton
+export default TagButton;
