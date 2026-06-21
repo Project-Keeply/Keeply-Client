7277ffb642ef8377c2268fcaf42af93ce51d4160
huniversal <hunivers0523@naver.com>
2026-06-21 21:16:02 +0900

    feat: 페이지 테스트 진행

--- src/pages/home/write/announcement-write-page.tsx
+++ src/pages/home/write/announcement-write-page.tsx
@@ -1,8 +1,10 @@
 import WritePageLayout from "@shared/layouts/WritePageLayout";
+import { ImgUploadButton } from '@shared/components';
 
 const AnnouncementWritePage = () => {
   return (
     <WritePageLayout label="공지사항 등록" onSubmit={() => {}}>
+      <ImgUploadButton onChange={() => {}} />
     </WritePageLayout>
   )
 }
