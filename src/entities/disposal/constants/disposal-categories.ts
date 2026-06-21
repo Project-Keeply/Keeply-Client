510eba5cca6d1eb5c748e858497e96114af5ff02
huniversal <hunivers0523@naver.com>
2026-06-21 22:54:30 +0900

    feat: Tag 버튼 컴포넌트 구현

--- src/entities/disposal/constants/disposal-categories.ts
+++ src/entities/disposal/constants/disposal-categories.ts
@@ -1,3 +1,3 @@
-  export const DISPOSAL_CATEGORIES = ['음료', '과자', '라면', 'FF'] as const;
+  export const DISPOSAL_CATEGORIES = ['음료', '과자', '라면', 'FF', '유제품', '기타'] as const;
   export type DisposalCategory = (typeof
   DISPOSAL_CATEGORIES)[number];
--- src/shared/components/TagButton.tsx
+++ src/shared/components/TagButton.tsx
@@ -1,5 +1,5 @@
 interface TagButtonProps {
-  lavbel: string;
+  label: string;
   isSelected: boolean;
   onClick: () => void;
 }
@@ -7,7 +7,7 @@ interface TagButtonProps {
 const TagButton = ({ label, isSelected, onClick }: TagButtonProps) => {
   return (
     <div>
-      <button>
+      <button className={`px-7 py-3 rounded-full text-body1 font-medium ${isSelected ? 'bg-primary-500 text-white' : 'bg-gray-200 text-gray-500'}`} onClick={onClick}>
         {label}
       </button>
     </div>
