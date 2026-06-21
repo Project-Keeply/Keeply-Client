b75a578a4165ae56d16c55a2dc3b4d08f0559890
huniversal <hunivers0523@naver.com>
2026-06-21 21:44:05 +0900

    test: 테스트 코드 제거

--- src/pages/home/write/announcement-write-page.tsx
+++ src/pages/home/write/announcement-write-page.tsx
@@ -1,20 +1,8 @@
-import { useState } from 'react';
-import { ImgUploadButton, TextArea } from '@shared/components';
-import WritePageLayout from "@shared/layouts/WritePageLayout";
+import WritePageLayout from '@shared/layouts/WritePageLayout';
 
 const AnnouncementWritePage = () => {
-  const [content, setContent] = useState('');
-
   return (
     <WritePageLayout label="공지사항 등록" onSubmit={() => {}}>
-      <TextArea
-        title="내용"
-        explanation="(상세 내용은 필수 사항이 아닙니다.)"
-        placeholder="공지사항 내용을 남겨주세요"
-        value={content}
-        onChange={setContent}
-      />
-      <ImgUploadButton onChange={() => {}} />
     </WritePageLayout>
   )
 }
