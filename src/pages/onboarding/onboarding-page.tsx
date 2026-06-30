5eccbead0fbaed61a8071547c2056b420d339edf
huniversal <hunivers0523@naver.com>
2026-07-01 01:38:52 +0900

    fix: 온보딩 성공 시 홈으로 이동

--- src/pages/onboarding/onboarding-page.tsx
+++ src/pages/onboarding/onboarding-page.tsx
@@ -1,9 +1,14 @@
+import { ROUTE_PATH } from '@shared/router/path';
+import { useNavigate } from 'react-router';
+
 import { OnboardingFunnel } from '@/features/onboarding';
 
 const OnboardingPage = () => {
+  const navigate = useNavigate();
+
   const handleOnboardingSuccess = () => {
-    // TODO: navigate(ROUTE_PATH.ONBOARDING_WELCOME, { replace: true });
-    console.log('Onboarding success');
+    // TODO: welcome 페이지 구현 후 ROUTE_PATH.ONBOARDING_WELCOME 으로 변경
+    navigate(ROUTE_PATH.HOME, { replace: true });
   };
 
   return <OnboardingFunnel onSuccess={handleOnboardingSuccess} />;
