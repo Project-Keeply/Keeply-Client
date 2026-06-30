a26f14d1833501f6014a59a0d6774292ea0eddba
huniversal <hunivers0523@naver.com>
2026-06-30 15:53:36 +0900

    style: 온모딩 공통 레이아웃 수치 변경

--- src/features/onboarding/components/OnboardingLayout.tsx
+++ src/features/onboarding/components/OnboardingLayout.tsx
@@ -26,12 +26,12 @@ const OnboardingLayout = ({
 }: OnboardingLayoutProps) => {
   return (
     <div className="mx-auto flex h-[100dvh] max-w-[430px] flex-col bg-white pb-[env(safe-area-inset-bottom)] pl-[env(safe-area-inset-left)] pr-[env(safe-area-inset-right)] pt-[env(safe-area-inset-top)]">
-      <header className="flex items-center px-5 py-5">
+      <header className="flex items-center px-5 py-[40px]">
         <IcLogo className="h-[47px] w-auto" />
       </header>
 
-      <div className="flex-1 overflow-y-auto px-5 pt-6">
-        <h1 className="text-headline2 mb-6 font-semibold whitespace-pre-line leading-[1.4]">
+      <div className="flex-1 overflow-y-auto px-5 pt-6]">
+        <h1 className="text-headline2 mb-6 pb-[45px] font-semibold whitespace-pre-line leading-[1.4]">
           {title}
         </h1>
         {children}
