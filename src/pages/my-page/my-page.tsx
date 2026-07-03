1b0d9225e181f770e8be0f926074fb54961d72ba
huniversal <hunivers0523@naver.com>
2026-07-03 14:55:24 +0900

    feat: 마이 페이지 훅 적용

--- src/pages/my-page/my-page.tsx
+++ src/pages/my-page/my-page.tsx
@@ -1,22 +1,21 @@
 import { CommonHeader, MenuRow } from '@shared/components';
-import { type Routes } from '@shared/router/path';
-import { useNavigate } from 'react-router';
+import useRouteNavigation from '@shared/hooks/use-route-navigation';
 
 import { MYPAGE_MENUS } from './config/menus';
 
 const MyPage = () => {
-  const navigate = useNavigate();
-
-  const handleMenuClick = (path: Routes) => {
-    navigate(path);
-  };
+  const { handleNavigate } = useRouteNavigation();
 
   return (
     <>
       <CommonHeader title="마이페이지" isSticky />
       <nav>
         {MYPAGE_MENUS.map(({ label, path }) => (
-          <MenuRow key={path} label={label} onClick={() => handleMenuClick(path)} />
+          <MenuRow
+            key={path}
+            label={label}
+            onClick={() => handleNavigate(path)}
+          />
         ))}
       </nav>
     </>
