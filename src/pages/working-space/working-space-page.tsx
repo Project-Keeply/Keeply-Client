b25ff02c33fca33e2485bc7b8e3df7baf6477ae5
huniversal <hunivers0523@naver.com>
2026-07-06 10:50:37 +0900

    feat: working-space 조립

--- src/pages/working-space/working-space-page.tsx
+++ src/pages/working-space/working-space-page.tsx
@@ -1,12 +1,31 @@
 import { CommonHeader } from '@shared/components';
+import FloatingButton from '@shared/components/FloatingButton';
 
 import WorkingLogList from '@/entities/working-space/components/WorkingLogList';
+import { WorkingLogWriteForm, useWorkingLogWrite } from '@/features/working-space';
 
 const WorkingSpacePage = () => {
+  const { isWriting, content, isValid, openWrite, closeWrite, handleContentChange, submit } =
+    useWorkingLogWrite();
+
   return (
     <>
       <CommonHeader title="근무일지" isSticky />
-      <WorkingLogList logs={[]} />
+      <WorkingLogList
+        logs={[]}
+        writeSlot={
+          isWriting && (
+            <WorkingLogWriteForm
+              content={content}
+              isValid={isValid}
+              onChange={handleContentChange}
+              onSubmit={submit}
+              onCancel={closeWrite}
+            />
+          )
+        }
+      />
+      <FloatingButton onClick={openWrite} />
     </>
   );
 };
