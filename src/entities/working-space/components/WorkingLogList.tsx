944c9257e1dcc9b3432bfe0857d83e20e98d0121
huniversal <hunivers0523@naver.com>
2026-07-06 10:48:51 +0900

    feat: WorkingList에 writeSlot props 추가

--- src/entities/working-space/components/WorkingLogList.tsx
+++ src/entities/working-space/components/WorkingLogList.tsx
@@ -1,4 +1,5 @@
 import { IcLeftArrow, IcRightArrow } from '@shared/icons/svgs';
+import type { ReactNode } from 'react';
 
 import useWorkingLogDate from '../hooks/use-working-log-date';
 import type { WorkingLog as WorkingLogType } from '../types/working-log';
@@ -6,9 +7,10 @@ import WorkingLog from './WorkingLog';
 
 interface WorkingLogListProps {
   logs: WorkingLogType[];
+  writeSlot?: ReactNode;
 }
 
-const WorkingLogList = ({ logs }: WorkingLogListProps) => {
+const WorkingLogList = ({ logs, writeSlot }: WorkingLogListProps) => {
   const { displayDate, isAtMin, isAtMax, handlePrevClick, handleNextClick, filteredLogs } =
     useWorkingLogDate(logs);
 
@@ -24,6 +26,7 @@ const WorkingLogList = ({ logs }: WorkingLogListProps) => {
         </button>
       </div>
       <div className="flex flex-col flex-1 min-h-0 overflow-y-auto">
+        {writeSlot}
         {filteredLogs.map(log => (
           <WorkingLog
             key={log.id}
