112dce38ff17398d8db246952f68260fff827575
huniversal <hunivers0523@naver.com>
2026-07-07 21:08:18 +0900

    feat: use-user 훅 세팅

---
+++ src/entities/user/hooks/use-user.ts
@@ -0,0 +1,8 @@
+import { USER_MOCK } from '../configs/user-mock';
+
+const useUser = () => {
+  // TODO: 사용자 조회 API 연동 후 mock 데이터를 대체한다.
+  return { user: USER_MOCK };
+};
+
+export default useUser;
