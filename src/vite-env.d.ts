7175e2c5d03dcd2ba0aca0a559a9174a01da8789
huniversal <hunivers0523@naver.com>
2026-07-08 13:19:34 +0900

    fix: 카카오 키 네이밍 수정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="vite-plugin-svgr/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_BASE_URL: string
-  readonly VITE_KAKAO_REST_API_KEY: string
-  readonly VITE_REDIRECT_URI: string
+  readonly VITE_API_BASE_URL: string;
+  readonly VITE_KAKAO_REST_API_KEY: string;
+  readonly VITE_KAKAO_REDIRECT_URI: string;
 }
 
 interface ImportMeta {
