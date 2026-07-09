dc668c6205ab93d4e5ca185e3a33cbca82707262
huniversal <hunivers0523@naver.com>
2026-07-09 10:25:36 +0900

    refactor: 배럴패턴 적용

--- src/features/onboarding/index.ts
+++ src/features/onboarding/index.ts
@@ -1,1 +1,9 @@
+export { postOwnerOnboarding, postStaffOnboarding } from './apis/onboarding-api';
 export { default as OnboardingFunnel } from './components/OnboardingFunnel';
+export { useOnboarding } from './hooks/use-onboarding';
+export type {
+  OwnerOnboardingRequest,
+  OwnerOnboardingResponse,
+  StaffOnboardingRequest,
+  StaffOnboardingResponse,
+} from './types';
