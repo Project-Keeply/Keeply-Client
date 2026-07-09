65b8302a0818acf34493f507c60aa3c4901f1a10
huniversal <hunivers0523@naver.com>
2026-07-09 15:07:31 +0900

    refactor: 로그아웃 훅 호출

--- src/features/my-page/components/ProfileInfoSection.tsx
+++ src/features/my-page/components/ProfileInfoSection.tsx
@@ -1,16 +1,9 @@
-import useRouteNavigation from '@shared/hooks/use-route-navigation';
-import { ROUTE_PATH } from '@shared/router/path';
-
 import { useUser } from '@/entities/user';
+import { useLogOut } from '@/features/auth/hooks/use-logout';
 
 const ProfileInfoSection = () => {
   const { user } = useUser();
-  const { handleNavigate } = useRouteNavigation();
-
-  const handleLogoutClick = () => {
-    // TODO: 로그아웃 API 연동 후 토큰/사용자 상태를 초기화한다.
-    handleNavigate(ROUTE_PATH.LOGIN);
-  };
+  const { mutate: handleLogOut, isPending} = useLogOut()
 
   return (
     <section className="flex items-center gap-6 px-5 py-7">
@@ -32,9 +25,7 @@ const ProfileInfoSection = () => {
       )}
 
       <div className="min-w-0 flex-1">
-        <p className="truncate text-title1 font-bold text-black">
-          {user.name}
-        </p>
+        <p className="truncate text-title1 font-bold text-black">{user.name}</p>
         {/* TODO: /users/me 에 groupName 추가 후 매장명 표시 복구
         <p className="mt-2.5 truncate text-body2 text-gray-300">
           {user.storeName}
@@ -44,7 +35,8 @@ const ProfileInfoSection = () => {
       <button
         type="button"
         className="shrink-0 cursor-pointer text-label2 font-medium text-gray-300"
-        onClick={handleLogoutClick}
+        disabled={isPending}
+        onClick={() => handleLogOut()}
       >
         로그아웃
       </button>
