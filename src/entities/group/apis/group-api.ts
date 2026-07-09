50525b6dd58711b9db28f8ced570ad0221765dd5
huniversal <hunivers0523@naver.com>
2026-07-09 10:52:23 +0900

    fix: lint 수정

--- src/entities/group/apis/group-api.ts
+++ src/entities/group/apis/group-api.ts
@@ -1,7 +1,7 @@
+import type { MyGroupResponse } from '../types/group';
+
 import type { ApiResponse } from '@/shared/apis';
 import { apiInstance, unwrapDataResponse } from '@/shared/apis';
-
-import type { MyGroupResponse } from '../types/group';
 
 export const getMyGroup = async (): Promise<MyGroupResponse> => {
   const res = await apiInstance.get<ApiResponse<MyGroupResponse>>('/groups/me');
