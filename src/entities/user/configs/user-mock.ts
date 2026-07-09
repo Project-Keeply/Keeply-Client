914686f14269c2ea549a5f117f78dcdc7755618c
huniversal <hunivers0523@naver.com>
2026-07-09 10:18:44 +0900

    feat: 온보딩 훅 구현

--- src/entities/user/configs/user-mock.ts
+++ src/entities/user/configs/user-mock.ts
@@ -1,7 +1,7 @@
 import type { User } from '../types/user';
 
 export const USER_MOCK: User = {
-  name: '홍길동',
-  storeName: 'Keeply 편의점',
+  name: '이훈진',
+  storeName: '',
   profileImageUrl: '',
 };
---
+++ src/features/onboarding/hooks/use-onboarding.ts
@@ -0,0 +1,32 @@
+import { useMutation } from '@tanstack/react-query';
+
+import { postOwnerOnboarding, postStaffOnboarding } from '../apis/onboarding-api';
+import { ONBOARDING_ROLE } from '../constants';
+import type { OnboardingFormValues } from '../schemas';
+
+import { useUser } from '@/entities/user';
+
+interface UseOnboardingOptions {
+  onSuccess: () => void;
+}
+
+export const useOnboarding = ({ onSuccess }: UseOnboardingOptions) => {
+  const { user } = useUser();
+
+  return useMutation({
+    mutationFn: (values: OnboardingFormValues) => {
+      if(values.role == ONBOARDING_ROLE.PART_TIMER){
+        return postStaffOnboarding({
+          name: user.name,
+          inviteCode: values.workspaceCode,
+        });
+      }
+      return postOwnerOnboarding({
+        name: user.name,
+        storeBrand: values.brand,
+        storeName: values.storeName,
+      })
+    },
+    onSuccess
+  })
+};
