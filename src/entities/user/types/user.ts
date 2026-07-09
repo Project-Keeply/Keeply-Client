770e779d1b26495d4185a52c75e0ee054a5c4bab
huniversal <hunivers0523@naver.com>
2026-07-09 14:20:26 +0900

    feat: User 스키마 파생 타입 추가

--- src/entities/user/types/user.ts
+++ src/entities/user/types/user.ts
@@ -1,3 +1,8 @@
+
+import type { components } from '@/shared/types/schema';
+
+export type UserResponse = components['schemas']['UserResponse'];
+
 export interface User {
   name: string;
   storeName: string;
