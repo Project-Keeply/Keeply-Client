d3183a8ffd5a0a685ee91875489cf9304ef014be
huniversal <hunivers0523@naver.com>
2026-07-11 17:45:13 +0900

    feat: 공지사항 작성 api

--- src/entities/announcement/apis/announcement-api.ts
+++ src/entities/announcement/apis/announcement-api.ts
@@ -4,6 +4,8 @@ import type { components } from '@/shared/types/schema';
 
 type PageResponseNoticeList =
   components['schemas']['PageResponseNoticeListResponse'];
+type CreateNoticeRequest = components['schemas']['CreateNoticeRequest'];
+type NoticeResponse = components['schemas']['NoticeResponse'];
 
 export const getNoticeList = async (
   groupId: number,
@@ -11,6 +13,17 @@ export const getNoticeList = async (
   const res = await apiInstance.get<ApiResponse<PageResponseNoticeList>>(
     `/groups/${groupId}/notices`,
     {params: {active: true, page: 0, size: 100}}
+  );
+  return unwrapDataResponse(res);
+}
+
+export const createNotice = async (
+  groupId: number,
+  body: CreateNoticeRequest,
+): Promise<NoticeResponse> => {
+  const res = await apiInstance.post<ApiResponse<NoticeResponse>>(
+    `/groups/${groupId}/notices`,
+    body,
   );
   return unwrapDataResponse(res);
 }
