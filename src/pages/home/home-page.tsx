495e3801d29fc0ad2e25380ffce60e2d1cf67c8f
huniversal <hunivers0523@naver.com>
2026-07-11 17:32:05 +0900

    feat: 더미데이터 제거 및 훅 연결

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -4,52 +4,23 @@ import { useNavigate } from 'react-router';
 
 import BannerCarousel from './ui/banner-carousel';
 
-import type { Announcement } from '@/entities/announcement';
-import { AnnouncementItemList } from '@/entities/announcement';
+import { AnnouncementItemList, useAnnouncements } from '@/entities/announcement';
+import { useMyGroup } from '@/entities/group';
 import banner1 from '@/shared/assets/images/banners/banner-1.svg';
 import banner2 from '@/shared/assets/images/banners/banner-2.svg';
 import banner3 from '@/shared/assets/images/banners/banner-3.svg';
 import ManagementGuideButton from '@/shared/components/ManagementGuideButton';
 import { IcLogo } from '@/shared/icons';
 
-const MOCK_ANNOUNCEMENTS: Announcement[] = [
-  {
-    id: 1,
-    tag: '주간',
-    title:
-      '종량제 봉투 판매 수량 제한종량제 봉투 판매 수량 제한종량제 봉투 판매 수량 제한종량제 봉투 판매 수량 제한',
-    imgUrl: '',
-    content: '봉투 판매 수량을 1인당 최대 2개로 제한합니다.',
-  },
-  {
-    id: 2,
-    tag: '주간',
-    title: '종량제 봉투 판매 수량 제한',
-    imgUrl: '',
-    content: '봉투 판매 수량을 1인당 최대 2개로 제한합니다.',
-  },
-  {
-    id: 3,
-    tag: '일일',
-    title: '종량제 봉투 판매 수량 제한',
-    imgUrl: '',
-    content: '봉투 판매 수량을 1인당 최대 2개로 제한합니다.',
-  },
-  {
-    id: 4,
-    tag: '주간',
-    title: '종량제 봉투 판매 수량 제한',
-    imgUrl: '',
-    content: '봉투 판매 수량을 1인당 최대 2개로 제한합니다.',
-  },
-];
 
 const HomePage = () => {
   const navigate = useNavigate();
-
   const handleProfileClick = () => {
     navigate(ROUTE_PATH.MYPAGE);
   };
+  
+  const { groupId } = useMyGroup();
+  const { announcements } = useAnnouncements(groupId);
 
   return (
     <div className="flex flex-1 flex-col bg-lightgray">
@@ -68,7 +39,7 @@ const HomePage = () => {
       />
       <div className="flex flex-col gap-5 px-6.25 pb-6.25">
         <BannerCarousel images={[banner1, banner2, banner3]} />
-        <AnnouncementItemList date={new Date()} items={MOCK_ANNOUNCEMENTS} />
+        <AnnouncementItemList date={new Date()} items={announcements} />
         <ManagementGuideButton />
       </div>
     </div>
