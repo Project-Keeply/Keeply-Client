8224b13202435eeb4ec4132d0bc00a9931c8b74f
huniversal <hunivers0523@naver.com>
2026-07-11 17:11:54 +0900

    feat: tag명 convert 함수 구현

---
+++ src/entities/announcement/utils/convert-announcement.ts
@@ -0,0 +1,12 @@
+import type { Announcement } from '../types/announcement';
+
+import type { components } from '@/shared/types/schema';
+
+type NoticeListResponse = components['schemas']['NoticeListResponse'];
+
+export const convertToAnnouncement = (notice: NoticeListResponse): Announcement => ({
+  id: notice.noticeId ?? 0,
+  tag: notice.tag == 'WEEKLY' ? '주간' : '일일',
+  imgUrl: notice.imageUrl ?? '',
+  title: notice.title ?? '',
+})
