347f83bc221cf54d74c2b1542b349856ca5c552c
huniversal <hunivers0523@naver.com>
2026-07-11 16:55:48 +0900

    feat: Notice get api 구현 진행

---
+++ src/entities/announcement/apis/annuouncement-api.ts
@@ -0,0 +1,16 @@
+import type { ApiResponse } from '@/shared/apis';
+import { apiInstance, unwrapDataResponse } from '@/shared/apis';
+import type { components } from '@/shared/types/schema';
+
+type PageResponseNoticeList =
+  components['schemas']['PageResponseNoticeListResponse'];
+
+export const getNoticeList = async (
+  groupId: number,
+): Promise<PageResponseNoticeList> => {
+  const res = await apiInstance.get<ApiResponse<PageResponseNoticeList>>(
+    `/groups/${groupId}/notices`,
+    {params: {active: true, page: 0, size: 20}}
+  );
+  return unwrapDataResponse(res);
+}
