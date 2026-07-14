151a4ba30130f9daf9025b892cbe53e7877ec0db
huniversal <hunivers0523@naver.com>
2026-07-14 16:05:59 +0900

    feat: 마이페이지 상세 메뉴 라우팅 추가

--- src/pages/my-page/config/menus.ts
+++ src/pages/my-page/config/menus.ts
@@ -8,4 +8,5 @@ interface MyPageMenu {
 export const MYPAGE_MENUS: MyPageMenu[] = [
   { label: '약관 및 정책', path: ROUTE_PATH.MYPAGE_TERMS },
   { label: '탈퇴하기', path: ROUTE_PATH.MYPAGE_WITHDRAW },
+  { label: '초대코드', path: ROUTE_PATH.MYPAGE_INVITE},
 ];
