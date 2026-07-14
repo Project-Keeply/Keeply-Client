fc303e77af34667fd587b49a2d7f745a7c432d02
huniversal <hunivers0523@naver.com>
2026-07-14 16:03:57 +0900

    feat: invite 상세 페이지 구현

--- src/pages/my-page/invite/invite-page.tsx
+++ src/pages/my-page/invite/invite-page.tsx
@@ -1,5 +1,13 @@
+import { InviteCodeCard, useMyGroup } from '@/entities/group';
+import DetailPageLayout from '@/shared/layouts/DetailPageLayout';
+
 const InvitePage = () => {
-  return <></>;
+  const { group } = useMyGroup();
+  return (
+    <DetailPageLayout title="가입코드">
+      <InviteCodeCard inviteCode={group.inviteCode ?? ''}/>
+    </DetailPageLayout>
+  );
 };
 
 export default InvitePage;
