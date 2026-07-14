7cd9c26691faa543eb317696a9b12d8fe627e7f2
huniversal <hunivers0523@naver.com>
2026-07-14 10:32:08 +0900

    feat: 근무일지 작성 api 세팅

--- src/entities/working-space/apis/work-log-api.ts
+++ src/entities/working-space/apis/work-log-api.ts
@@ -3,6 +3,8 @@ import { apiInstance, unwrapDataResponse } from '@/shared/apis';
 import type { components } from '@/shared/types/schema';
 
 type PageResponseWorkLog = components['schemas']['PageResponseWorkLogResponse'];
+type CreateWorkLogRequest = components['schemas']['CreateWorkLogRequest'];
+type WorkLogResponse = components['schemas']['WorkLogResponse']
 
 export const getWorkLogList = async (
   groupId: number,
@@ -12,6 +14,17 @@ export const getWorkLogList = async (
   const res = await apiInstance.get<ApiResponse<PageResponseWorkLog>>(
     `/groups/${groupId}/work-logs`,
     { params: {from, to, page: 0, size: 100}},
+  );
+  return unwrapDataResponse(res);
+}
+
+export const createWorkLog = async (
+  groupId: number,
+  body: CreateWorkLogRequest,
+): Promise<WorkLogResponse> => {
+  const res = await apiInstance.post<ApiResponse<WorkLogResponse>>(
+    `/groups/${groupId}/work-logs`,
+    body,
   );
   return unwrapDataResponse(res);
 }
