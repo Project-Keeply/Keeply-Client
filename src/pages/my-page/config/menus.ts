f057fe640ebdf515c9b687d77ceb268f69e75968
huniversal <hunivers0523@naver.com>
2026-07-14 16:12:56 +0900

    feat: invite 페이지 ownerOnly 추가 및 위치 변경

--- src/pages/my-page/config/menus.ts
+++ src/pages/my-page/config/menus.ts
@@ -3,10 +3,11 @@ import { ROUTE_PATH, type Routes } from '@shared/router/path';
 interface MyPageMenu {
   label: string;
   path: Routes;
+  ownerOnly?: boolean;
 }
 
 export const MYPAGE_MENUS: MyPageMenu[] = [
-  { label: '약관 및 정책', path: ROUTE_PATH.MYPAGE_TERMS },
+  { label: '초대코드', path: ROUTE_PATH.MYPAGE_INVITE, ownerOnly: true},
   { label: '탈퇴하기', path: ROUTE_PATH.MYPAGE_WITHDRAW },
-  { label: '초대코드', path: ROUTE_PATH.MYPAGE_INVITE},
+  { label: '약관 및 정책', path: ROUTE_PATH.MYPAGE_TERMS },
 ];
