ee0014e4d55ac36b411c1ae52a75b4b1086381f0
huniversal <hunivers0523@naver.com>
2026-07-16 11:24:51 +0900

    feat: 전역 토스터 마운트

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { Suspense } from 'react'
-import { router } from '@shared/router/router'
-import { RouterProvider } from 'react-router'
+import { Suspense } from 'react';
+import { router } from '@shared/router/router';
+import { RouterProvider } from 'react-router';
+import { Toaster } from 'sonner';
 
-import QueryProvider from './app/providers/query-provider'
+import QueryProvider from './app/providers/query-provider';
 
 const App = () => {
   return (
@@ -10,8 +11,12 @@ const App = () => {
       <Suspense fallback={null}>
         <RouterProvider router={router} />
       </Suspense>
+      <Toaster
+        position="top-center"
+        toastOptions={{ unstyled: true, classNames: { toast: 'w-full' } }}
+      />
     </QueryProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
