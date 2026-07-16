710ce577cf9e230a0214a55145962cdde4c6d735
huniversal <hunivers0523@naver.com>
2026-07-16 11:24:30 +0900

    sonner 토스트 진입/이탈 조정

--- src/features/onboarding/hooks/use-onboarding.ts
+++ src/features/onboarding/hooks/use-onboarding.ts
@@ -1,3 +1,4 @@
+import { showErrorToast } from '@shared/utils/toast';
 import { useMutation } from '@tanstack/react-query';
 
 import {
@@ -31,10 +32,8 @@ export const useOnboarding = ({ onSuccess }: UseOnboardingOptions) => {
       });
     },
     onSuccess,
-    // TODO: 토스트/에러 문구 UX 컴포넌트 추가 후 사용자 피드백(isError)으로 대체
-    onError: (error) => {
-      // eslint-disable-next-line no-console
-      console.error('온보딩 제출 실패:', error);
+    onError: () => {
+      showErrorToast('온보딩에 실패했어요. 잠시 후 다시 시도해주세요.');
     },
   });
 };
